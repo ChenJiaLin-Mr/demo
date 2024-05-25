8f1c1c2cb7c2483b31f6d0547908d0e2ed3e5f02
My-function <anxingtian4@gmail.com>
2024-05-25 15:34:48 +0800

    feat： 关于van-action-sheet组件有概率不能弹出

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <van-tabbar >
+      <van-tabbar-item icon="home-o" @click="funShow">标签</van-tabbar-item>
+    </van-tabbar>
+    <van-action-sheet
+      v-model="actionSheet"
+      :actions="actions"
+      close-on-click-action
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      actionSheet:false,
+      actions: [{ name: "选项一" }, { name: "选项二" }, { name: "选项三" }],
+    };
+  },
+  methods: {
+    // 在正式环境， 这个事件有小概率会失效， 导致弹窗不能展示出来，目前已知手机型号有小米，苹果13
+    funShow() {
+      this.actionSheet = true;
+    },
+  },
+};
+</script>
+
+<style></style>
